92e22606e84070786d939793dba815a90a0f17b8
Artur <arttturyyy@gmail.com>
2024-09-12 18:27:28 +0200

    Db address changed

--- src/test/java/com/artur/MainTest.java
+++ src/test/java/com/artur/MainTest.java
@@ -3,10 +3,12 @@ package com.artur;
 import jakarta.transaction.Transactional;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 
 @Transactional
 @Rollback
 @SpringBootTest
+@ActiveProfiles("dev")
 public class MainTest {
 
 }
